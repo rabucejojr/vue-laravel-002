5d3508f484deff9dcfbea721107b0663aef8c7c6
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-11-13 15:06:31 +0800

    fixed and moved search component to component folder

---
+++ resources/js/Components/Search.vue
@@ -0,0 +1,14 @@
+<script setup>
+const emit = defineEmits(['searchInput']);
+
+const searchInput = (e) =>{
+    // console.log(e.target.value);
+    emit('searchInput',e.target.value);
+}
+
+</script>
+<template>
+    <form >
+        <input type="text" @input="searchInput" placeholder="Search" class="mr-3 border rounded">
+    </form>
+</template>
